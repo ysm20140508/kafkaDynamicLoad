22b3bb0e096a657009c7b45fc37b81be78d73d59
ysm20140508 <1247793952@qq.com>
2015-10-24 17:35:29 +0800

    add zk  support   Distributed

--- src/main/java/com/yunva/utill/SysContant.java
+++ src/main/java/com/yunva/utill/SysContant.java
@@ -4,6 +4,6 @@ package com.yunva.utill;
  * Created by Administrator on 2015-08-29.
  */
 public class SysContant {
-      public final  static Integer INSERTSIZE=10;
+      public final  static Integer INSERTSIZE=1000;
       public final  static Integer INSERTHEATBEAT=1000*60;
 }
